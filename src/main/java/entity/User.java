e659948c7b3aed743f52e9328bf3553a8106c1ad
ANKIT RAJ GUPTA <raj.ankit744@gmail.com>
2018-04-26 03:12:05 +0530

    user database contact field changed to string, sign up and login completed, seller page logout button, session variables added : login(to ensure only one can login in one session), cur(to store the current url), username(to show the uid when seller login),

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,20 +1,66 @@
 package entity;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import helpers.DatabaseConnect;
 import java.util.ArrayList;
 
 public class User {
-	String uid, password, type, address;
-	long contact;
+	String uid, password, name, type, address, contact;
 	ArrayList<Order> order = new ArrayList<>();
-	public User(String uid, String password, String type, String address, long contact,ArrayList<Order> order) {
+	public User(String uid, String password, String name, String type, String address, String contact,ArrayList<Order> order) {
 		this.uid = uid;
 		this.password =  password;
+		this.name = name;
 		this.type = type;
 		this.address = address;
 		this.contact = contact;
 		this.order = order;
 	}
-	
+	public void register() { 
+		try {
+			Connection conn = DatabaseConnect.createInstance().mySqlConnection();
+			String query = "insert into user (uid, password, name, usertype, contact, address)" 
+					+ " values (?,?,?,?,?,?)";
+			
+			if(conn == null)	
+				return ;
+			PreparedStatement ps = conn.prepareStatement(query);
+			ps.setString(1, this.uid);
+			ps.setString(2, this.password);
+			ps.setString(3, this.name);
+			ps.setString(4, this.type);
+			ps.setString(5, this.contact);
+			ps.setString(6, this.address);
+			ps.execute();
+			conn.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public String verifyuser() {
+		try {
+			Connection conn = DatabaseConnect.createInstance().mySqlConnection();
+			String query = "Select * from user where uid = ?"; 
+			
+			if(conn == null)	
+				return "Invalid";
+			PreparedStatement ps = conn.prepareStatement(query);
+			ps.setString(1,this.uid);
+			ResultSet rs = ps.executeQuery();
+			if(!rs.next())	
+				return "Invalid";
+			if(rs.getString(2).equals(this.password))
+				return rs.getString(4);
+			rs.close();
+			conn.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "Invalid";
+	}
 	public String getUid() {
 		return uid;
 	}
@@ -27,7 +73,7 @@ public class User {
 	public String getAddress() {
 		return address;
 	}
-	public long getContact() {
+	public String getContact() {
 		return contact;
 	}
 }
